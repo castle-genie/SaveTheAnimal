060f294696071373b4a2c87e7085ea1639f1c3fb
castle-genie <castlegenie4719@gamil.com>
2024-03-11 17:41:17 +0900

    volunteer page

---
+++ src/main/java/com/spring/volunteer/dao/VolunteerDAO.java
@@ -0,0 +1,14 @@
+package com.spring.volunteer.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.spring.volunteer.vo.VolunteerVO;
+
+@Mapper
+public interface VolunteerDAO {
+	public List<VolunteerVO> volunteerList(VolunteerVO volunteerVO);
+	
+	public VolunteerVO volunteerDetail(VolunteerVO volunteerVO);
+}
