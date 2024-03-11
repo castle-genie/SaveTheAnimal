857c3a38a46dc51cf8d404893caf821a6f643719
choisuin <choisuin000327@gmail.com>
2024-03-12 00:18:28 +0900

    re commit for reportForm

--- src/main/java/com/spring/sanction/controller/SanctionController.java
+++ src/main/java/com/spring/sanction/controller/SanctionController.java
@@ -8,17 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.sanction.service.SanctionService;
 import com.spring.sanction.vo.SanctionVO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/admin/sanction/*")
 @Controller
 public class SanctionController {
 	
 	@Autowired
 	private SanctionService sanctionService;
-	
+		
 	@PostMapping("sanctionInsert")
 	public String sanctionInsert(SanctionVO svo) {
+		log.info("sanctionInsert 호출");
+		
+
 		sanctionService.sanctionInsert(svo);
-		return "redirect:/admin/report/reportList";
+
+		return "redirect:/admin/report/reportList/";
 	}
 	
 	
--- src/main/resources/static/include/js/reportDetail.js
+++ src/main/resources/static/include/js/reportDetail.js
@@ -1,19 +1,52 @@
 $(function(){
 	
 	$("#sanctionBtn").on("click",() =>{
-		//console.log("제재버튼 확인");
-		$("#f_data").attr({
-			"method":"post",
-			"action":"/admin/report/reportUpdate"
+		console.log("제재버튼 확인");
+
+		$.ajax({
+			type:"post",
+			url:"/admin/sanction/sanctionInsert",
+			data:$("#f_data").serialize(),
+			success:function(){
+				console.log("성공");
+			},
+			error:function(){
+				console.log("실패");
+				$("#f_data").submit();
+			}
 		});
-		$("#f_data").attr({
+/*		$.ajax({
+			type:"post",
+			url:"/admin/report/reportUpdate",
+			data:$("#f_data").serialize(),
+			success:function(){
+				console.log("성공");
+			},
+			error:function(){
+				console.log("실패");
+				$("#f_data").submit();
+			}
+		});
+*/		
+		
+
+
+
+
+
+/*		$("#f_data").attr({
 			"method":"post",
 			"action":"/admin/sanction/sanctionInsert"
 		});
-		
-		
-		
 		$("#f_data").submit();
+*/
+
+		$("#f_data").attr({
+			"method":"post",
+			"action":"/admin/report/reportUpdate",
+		});
+		$("#f_data").submit();
+		
 	});
 	
 	
