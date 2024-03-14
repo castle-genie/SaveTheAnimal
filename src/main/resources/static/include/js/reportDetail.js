3bdf401c3f7d6f73168d60cc0c8d48b1f66d2194
choisuin <choisuin000327@gmail.com>
2024-03-15 01:17:04 +0900

    code sorting in reportDetail.js

--- src/main/resources/static/include/js/reportDetail.js
+++ src/main/resources/static/include/js/reportDetail.js
@@ -7,136 +7,66 @@ $(function(){
 	const afcomment = '입양후이야기 댓글';
 	const boardSort = document.getElementById("boardSort").innerText;
 
-$("#sanctionBtn").on("click", () => {
-    // AJAX 요청을 수행하고 프로미스를 반환하는 함수 정의
-    function ajaxRequest(url, data) {
-        return new Promise(function(resolve, reject) {
-            $.ajax({
-                type: "post",
-                url: url,
-                data: data,
-                success: function(response) {
-                    resolve(response);
-                },
-                error: function(xhr, status, error) {
-                    reject(error);
-                }
-            });
-        });
-    }
-
-    // 첫 번째 AJAX 요청
-    ajaxRequest("/admin/sanction/sanctionInsert", $("#f_data").serialize())
-        .then(() => {
-            // boardSort에 따라 다른 AJAX 요청을 선택적으로 실행
-            let reportUpdateUrl;
-            switch (boardSort) {
-                case fboard:
-                    reportUpdateUrl = "/admin/report/reportUpdateFB";
-                    break;
-                case vfboard:
-                    reportUpdateUrl = "/admin/report/reportUpdateVB";
-                    break;
-                case afboard:
-                    reportUpdateUrl = "/admin/report/reportUpdateAB";
-                    break;
-                case fcomment:
-                    reportUpdateUrl = "/admin/report/reportUpdateFC";
-                    break;
-                case vfcomment:
-                    reportUpdateUrl = "/admin/report/reportUpdateVC";
-                    break;
-                case afcomment:
-                    reportUpdateUrl = "/admin/report/reportUpdateAC";
-                    break;
-                default:
-                    // 기타 경우에는 추가적인 AJAX 요청이 필요 없음
-                    return Promise.resolve();
-            }
-            // 선택된 AJAX 요청 실행
-            return ajaxRequest(reportUpdateUrl, $("#f_data").serialize());
-        })
-        .then(() => {
-            // AJAX 요청 성공 후 추가 작업 수행
-            console.log("test");
-            $("#f_data").attr({
-                "method": "post",
-                "action": "/admin/report/repcntUpdate"
-            });
-            // 폼 제출
-            $("#f_data").submit();
-        });
-});
-
+	$("#sanctionBtn").on("click", () => {
+	    // AJAX 요청을 수행하고 프로미스를 반환하는 함수 정의
+	    function ajaxRequest(url, data) {
+	        return new Promise(function(resolve, reject) {
+	            $.ajax({
+	                type: "post",
+	                url: url,
+	                data: data,
+	                success: function(response) {
+	                    resolve(response);
+	                },
+	                error: function(error) {
+	                    reject(error);
+	                }
+	            });
+	        });
+	    }
 	
-/*	$("#sanctionBtn").on("click",() =>{
-		//console.log("제재버튼 확인");
-		$.ajax({
-			type:"post",
-			url:"/admin/sanction/sanctionInsert",
-			data:$("#f_data").serialize(),
-			success:function(){
-				if(boardSort == fboard){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateFB",
-						data:$("#f_data").serialize(),
-						success:function(){
-							console.log("test");
-							$("f_data").attr({
-								"method":"post",
-								"action":"/admin/report/repcntUpdate"
-							});
-						},
-						error:function(){}
-					})
-				}else if(boardSort == vfboard){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateVB",
-						data:$("#f_data").serialize(),
-						success:function(){},
-						error:function(){}
-					})
-				}else if(boardSort == afboard){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateAB",
-						data:$("#f_data").serialize(),
-						success:function(){},
-						error:function(){}
-					})
-				}else if(boardSort == fcomment){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateFC",
-						data:$("#f_data").serialize(),
-						success:function(){},
-						error:function(){}
-					})
-				}else if(boardSort == vfcomment){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateVC",
-						data:$("#f_data").serialize(),
-						success:function(){},
-						error:function(){}
-					})
-				}else if(boardSort == afcomment){
-					$.ajax({
-						type:"post",
-						url:"/admin/report/reportUpdateAC",
-						data:$("#f_data").serialize(),
-						success:function(){},
-						error:function(){}
-					})
-				}
-			},
-			error:function(){}
-		});
-		$("#f_data").submit();
+	    // 첫 번째 AJAX 요청
+	    ajaxRequest("/admin/sanction/sanctionInsert", $("#f_data").serialize())
+	        .then(() => {
+	            // boardSort에 따라 다른 AJAX 요청을 선택적으로 실행
+	            let reportUpdateUrl;
+	            switch (boardSort) {
+	                case fboard:
+	                    reportUpdateUrl = "/admin/report/reportUpdateFB";
+	                    break;
+	                case vfboard:
+	                    reportUpdateUrl = "/admin/report/reportUpdateVB";
+	                    break;
+	                case afboard:
+	                    reportUpdateUrl = "/admin/report/reportUpdateAB";
+	                    break;
+	                case fcomment:
+	                    reportUpdateUrl = "/admin/report/reportUpdateFC";
+	                    break;
+	                case vfcomment:
+	                    reportUpdateUrl = "/admin/report/reportUpdateVC";
+	                    break;
+	                case afcomment:
+	                    reportUpdateUrl = "/admin/report/reportUpdateAC";
+	                    break;
+	                default:
+	                    // 기타 경우에는 추가적인 AJAX 요청이 필요 없음
+	                    return Promise.resolve();
+	            }
+	            // 선택된 AJAX 요청 실행
+	            return ajaxRequest(reportUpdateUrl, $("#f_data").serialize());
+	        })
+	        .then(() => {
+	            // AJAX 요청 성공 후 추가 작업 수행
+	            console.log("test");
+	            $("#f_data").attr({
+	                "method": "post",
+	                "action": "/admin/report/repcntUpdate"
+	            });
+	            // 폼 제출
+	            $("#f_data").submit();
+	        });
 	});
-*/	
 	
 	
 	$("#reportDeleteBtn").on("click",() =>{
