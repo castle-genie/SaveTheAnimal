829204cd3fe4fa1918d5a928cc6699cc0722f8e7
honggildong234 <yumie4w@gmail.com>
2024-03-14 18:00:00 +0900

    donate Update

--- src/main/java/com/spring/donate/controller/DonateController.java
+++ src/main/java/com/spring/donate/controller/DonateController.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.donate.vo.DonateVO;
+
 @Controller
 @RequestMapping("/donate")
 public class DonateController {
+	
+	
 	@GetMapping("/donatepay")
-	public String donatepay() {
+	public String donatepay(DonateVO dovo) {
 		return "/donate/donatePay";
 	}
+	
+	@GetMapping("/donateUnit")
+	public String donateUnit(DonateVO dovo) {
+		return "/donate/donateUnit";
+	}
+	
 }
