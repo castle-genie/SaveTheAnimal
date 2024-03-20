1357f822522ef25d73794e33853fe94676ff188c
choisuin <choisuin000327@gmail.com>
2024-03-20 13:22:03 +0900

    reportUpdate function success

--- src/main/resources/static/include/js/reportUpdate.js
+++ src/main/resources/static/include/js/reportUpdate.js
@@ -2,14 +2,10 @@ $(function(){
 	const userId = document.getElementById("userId").value;
 	
 	$("#fbReportUpdateBtn").on("click", () => {	
-		if(userId != "null"){	
-			$("#f_data").attr({
-				"method": "post",
-				"action": "/report/reportModify"
-			});
-			$("#f_data").submit();
-		}else{
-			alert("로그인 후 이용 가능한 서비스입니다.");
-		}
+		$("#f_data").attr({
+			"method": "post",
+			"action": "/report/reportModify"
+		});
+		$("#f_data").submit();
 	});
 })
