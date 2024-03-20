542316c0dcf6449989b4e3d82b89863adf400083
nohcy0313 <shckdduf123@gmail.com>
2024-03-20 11:40:03 +0900

    11:39

--- src/main/java/com/spring/comment/dao/FcommentDAO.java
+++ src/main/java/com/spring/comment/dao/FcommentDAO.java
@@ -8,31 +8,8 @@ import com.spring.comment.vo.FcommentVO;
 
 @Mapper
 public interface FcommentDAO {
-	/*
-	//댓글 조회
-	public List<FcommentVO> list(int fboardId) throws Exception;
-	
-	//댓글 작성
-	public void FcommentInsert(FcommentVO fcommentvo) throws Exception;
-	
-	//댓글 수정
-	public void FcommentUpdate(FcommentVO fcommentvo) throws Exception;
-	
-	//댓글 삭제
-	public void FcommentDelete(FcommentVO fcommentvo) throws Exception;
-	*/
-	
-	//댓글 조회
-	public List<FcommentVO> list(int fboardId) throws Exception;
-	
-	//댓글 작성
-	public FcommentVO FcommentInsert(FcommentVO fcommentvo) throws Exception;
-	
-	//댓글 수정
-	public FcommentVO FcommentUpdate(FcommentVO fcommentvo) throws Exception;
-	
-	//댓글 삭제
-	public FcommentVO FcommentDelete(FcommentVO fcommentvo) throws Exception;
+	public List<FcommentVO> fcommentList(FcommentVO fcommentvo);
+
 	 
 	
 	
